8c09d2618ac381f6f550b926db2eb05a6ce31d81
princekr1603 <princekr1603@gmail.com>
2023-08-07 00:43:45 +0530

    updated

---
+++ array/address.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5]={1,4,2,5,6};
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[1]);
+    printf("%p\n",&arr[2]);
+    printf("%p\n",&arr[3]);
+    printf("%p\n",&arr[4]);
+
+}
