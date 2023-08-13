a59c6980ffd52a56c908daa923ece3d9d06b01bf
princekr1603 <princekr1603@gmail.com>
2023-08-13 22:29:19 +0530

    updated

---
+++ array/basic1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int sub[50];
+    for(int i=0;i<=48;i++)
+    {
+        sub[i]=i;
+        printf("\n%d",sub[i]);
+    }
+}
--- array/basicarray.c
+++ array/basicarray.c
@@ -9,7 +9,7 @@
      }
     for (int i=0;i<5;i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d",arr[i]);
     }
     return 0;
   }
