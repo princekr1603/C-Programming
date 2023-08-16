3698a220ec194f67f1260dfd277b23a0dc15aed0
princekr1603 <princekr1603@gmail.com>
2023-08-16 23:56:54 +0530

    updated

---
+++ array/evenoddsum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arr[7]={1,2,3,4,5,6,7};
+    int sumeven=0;
+    int sumodd=0;
+    for (int i=0;i<=6;i++)
+    {
+        if(i%2==0){
+            sumeven+=arr[i];
+
+        }
+        else{
+            sumodd+=arr[i];
+        }
+    }
+    int res=sumeven-sumodd;
+    printf("%d",res);
+    return 0;
+
+}
